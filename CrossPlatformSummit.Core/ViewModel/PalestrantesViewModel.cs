b2ab6ee0b64facbbcf579e8b6989da2c1c941e26
William S. Rodriguez <williamsrz@outlook.com>
2015-09-03 23:31:25 -0300

    and a comma

--- CrossPlatformSummit.Core/ViewModel/PalestrantesViewModel.cs
+++ CrossPlatformSummit.Core/ViewModel/PalestrantesViewModel.cs
@@ -4,34 +4,37 @@ using CrossPlatformSummit.Core.Services;
 
 namespace CrossPlatformSummit.Core.ViewModel
 {
-	public class PalestrantesViewModel
-	{
-		public ObservableCollection<Model.Palestrante> Palestrantes {
-			get;
-		}
+    public class PalestrantesViewModel
+    {
+        public ObservableCollection<Model.Palestrante> Palestrantes
+        {
+            get;
+            set;
+        }
 
-		public ICommand CarregarCommand {
-			get;
-			set;
-		}
+        public ICommand CarregarCommand
+        {
+            get;
+            set;
+        }
 
-		public PalestrantesViewModel()
-		{
-			Palestrantes = new ObservableCollection<Model.Palestrante> ();
-			CarregarCommand = new RelayCommand (Carregar);
-		}
+        public PalestrantesViewModel()
+        {
+            Palestrantes = new ObservableCollection<Model.Palestrante>();
+            CarregarCommand = new RelayCommand(Carregar);
+        }
 
-		private void Carregar()
-		{
-		    var navigationService = Services.Services.IoC.Resolve<INavigationService>();
-		    navigationService.NavigateTo<PalestranteViewModel>();
+        private void Carregar()
+        {
+            var navigationService = Services.Services.IoC.Resolve<INavigationService>();
+            navigationService.NavigateTo<PalestranteViewModel>();
 
-		    //var quantidade = Palestrantes.Count;
-		    //Palestrantes.Add (new Model.Palestrante{
-		    //	Nome = string.Format("Nome {0}", quantidade),
-		    //	Bio = string.Format("Bio {0}", quantidade),
-		    //	UrlFoto = "https://avatars3.githubusercontent.com/u/13861424?v=3&s=200"
-		    //});
-		}
-	}
+            //var quantidade = Palestrantes.Count;
+            //Palestrantes.Add (new Model.Palestrante{
+            //	Nome = string.Format("Nome {0}", quantidade),
+            //	Bio = string.Format("Bio {0}", quantidade),
+            //	UrlFoto = "https://avatars3.githubusercontent.com/u/13861424?v=3&s=200"
+            //});
+        }
+    }
 }
